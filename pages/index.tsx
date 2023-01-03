59eb3b7c444548a1d24f3750c60a68411fc0ec29
Mihir <mihir@mihir.work>
2023-01-03 20:46:18 +0530

    Rome wasn't built in a day

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,20 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
 
-import styles from "../styles/home.module.css";
+import styles from '../styles/home.module.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>Mihir Desai</title>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-			</Head>
-			<main className={styles.main}>
-				<h1 className={inter.className}>Mihir Desai</h1>
-			</main>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Mihir Desai</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={inter.className}>Mihir Desai</h1>
+      </main>
+    </>
+  )
 }
